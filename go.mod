59608ee0e6dc8b3301885b8ef8954199b4bb9610
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-22 06:43:15 -0700

    Working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/user/dandybear
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"example/user/dandybear/morestrings"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
